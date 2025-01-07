ed22bb00d00695df0776aadad9c65bc3b5e43750
noarkhh <jakub.pryc@gmail.com>
2025-01-07 16:55:35 +0100

    Add stream format validation

--- lib/membrane_vpx/decoder/vpx_decoder.ex
+++ lib/membrane_vpx/decoder/vpx_decoder.ex
@@ -2,17 +2,9 @@ defmodule Membrane.VPx.Decoder do
   @moduledoc false
 
   require Membrane.Logger
-  alias Hex.State
-  alias Membrane.{Buffer, RawVideo, VP8, VP9}
+  alias Membrane.{Buffer, RawVideo, RemoteStream, VP8, VP9}
   alias Membrane.Element.CallbackContext
   alias Membrane.VPx.Decoder.Native
-
-  @type decoded_frame :: %{
-          payload: binary(),
-          pixel_format: RawVideo.pixel_format(),
-          width: non_neg_integer(),
-          height: non_neg_integer()
-        }
 
   defmodule State do
     @moduledoc false
@@ -65,6 +57,8 @@ defmodule Membrane.VPx.Decoder do
 
     stream_format_action =
       if new_stream_format != ctx.pads.output.stream_format do
+        validate_stream_formats(ctx.pads.input.stream_format, new_stream_format)
+
         [stream_format: {:output, new_stream_format}]
       else
         []
@@ -72,5 +66,29 @@ defmodule Membrane.VPx.Decoder do
 
     {stream_format_action ++
        [buffer: {:output, %Buffer{payload: decoded_frame.payload, pts: pts}}], state}
+  end
+
+  @spec validate_stream_formats(RemoteStream.t() | VP8.t() | VP9.t(), RawVideo.t()) ::
+          :ok
+  defp validate_stream_formats(input_stream_format, output_stream_format) do
+    case input_stream_format do
+      %RemoteStream{} ->
+        :ok
+
+      %{width: width, height: height} ->
+        if width != output_stream_format.width do
+          Membrane.Logger.warning(
+            "Image width specified in stream format: #{inspect(width)} differs from the real image width: #{inspect(output_stream_format.width)}, using the actual value."
+          )
+        end
+
+        if height != output_stream_format.height do
+          Membrane.Logger.warning(
+            "Image height specified in stream format: #{inspect(height)} differs from the real image height: #{inspect(output_stream_format.height)}, using the actual value."
+          )
+        end
+
+        :ok
+    end
   end
 end
